3697b03126adb5a8364501df60180aaa507ebf62
dwqs <461147874@qq.com>
2018-03-22 23:30:35 +0800

    refactor: Delete files

--- src/page/index.js
+++ src/page/index.js
@@ -1,10 +1,6 @@
-import 'normalize.css';
-import 'babel-polyfill';
-
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import { sync } from 'vuex-router-sync';
 
+import App from '../general/app/index.vue';
 
 const env = process.env.NODE_ENV || 'development';
 
@@ -13,37 +9,7 @@ if (env !== 'development') {
     Vue.config.productionTip = false;
 }
 
-import store from '../vuex/index';
-
-Vue.use(VueRouter);
-
-// dynamic import for on-demand loaded chunk
-const Info = () => import(/* webpackChunkName: "info" */ '@components/info/');
-const App = () => import(/* webpackChunkName: "main" */ '../general/app/index');
-
-const Outer = { template: '<router-view></router-view>' };
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            component: Outer,
-            children: [
-                // 嵌套路由 https://github.com/vuejs/vue-router/blob/next-doc/docs/en/advanced-routing/nested.md
-                { path: '', component: App },
-                { path: 'info', component: Info }
-            ]
-        }
-    ]
-});
-
-sync(store, router);
-
 const app = new Vue({
-    router,
-    store,
-    ...Outer
+    el: '#app',
+    render: h => h(App)
 });
-
-app.$mount('#app');
